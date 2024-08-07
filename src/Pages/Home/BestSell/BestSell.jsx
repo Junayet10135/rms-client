76c553ee9b2c5933a144573289ad8b5e16761141
JunayetTamim <junayettamim186@gmail.com>
2024-08-07 16:24:01 +0100

    hook implement

--- src/Pages/Home/BestSell/BestSell.jsx
+++ src/Pages/Home/BestSell/BestSell.jsx
@@ -1,17 +1,19 @@
-import { useEffect, useState } from 'react';
 import SectionTitle from '../../Components/SectionTitle';
 import Menu from '../../Shared/Items/Menu';
+import useMenu from '../../../hooks/useMenu';
 
 const BestSell = () => {
-    const [menu, setMenu] = useState([]);
-    useEffect(() => {
-        fetch('menu.json')
-            .then(res => res.json())
-            .then(data => {
-                const popularItems = data.filter(item => item.category === 'popular');
-                setMenu(popularItems)
-            })
-    }, [])
+    const [menu] = useMenu();
+    const popular = menu.filter(item => item.category === 'popular');
+    // const [menu, setMenu] = useState([]);
+    // useEffect(() => {
+    //     fetch('menu.json')
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             const popularItems = data.filter(item => item.category === 'popular');
+    //             setMenu(popularItems)
+    //         })
+    // }, [])
     return (
         <section className="mb-12">
             <SectionTitle
@@ -20,7 +22,7 @@ const BestSell = () => {
             ></SectionTitle>
             <div className="grid md:grid-cols-2 gap-10">
                 {
-                    menu.map(item => <Menu
+                    popular.map(item => <Menu
                         key={item._id}
                         item={item}
                     ></Menu>)
