b3f0f89b5fb8fdd8ff8fe018fc21f28af212eed3
JunayetTamim <junayettamim186@gmail.com>
2024-08-07 16:33:54 +0100

    todays offer

--- src/Pages/FullMenu/Fullmenu.jsx
+++ src/Pages/FullMenu/Fullmenu.jsx
@@ -1,14 +1,24 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../Shared/Cover/Cover';
 import CoverMenu from '../../assets/menu/banner3.jpg'
+import useMenu from '../../hooks/useMenu';
+import SectionTitle from '../Components/SectionTitle';
+import MenuTypes from './MenuTypes';
 const Fullmenu = () => {
+    const [menu] = useMenu();
+    const desserts = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const offered = menu.filter(item => item.category === 'offered');
     return (
         <div>
             <Helmet>
                 <title>Full Menu</title>
             </Helmet>
             <Cover img={CoverMenu} title="Our Menu"></Cover>
-
+            <SectionTitle subHeading="Don't Miss" heading="Today's Offer"></SectionTitle>
+            <MenuTypes items={offered}></MenuTypes>
         </div>
     );
 };
--- src/Pages/FullMenu/MenuTypes.jsx
+++ src/Pages/FullMenu/MenuTypes.jsx
@@ -1,8 +1,16 @@
+import Menu from "../Shared/Items/Menu";
 
-const MenuTypes = () => {
+const MenuTypes = ({ items }) => {
     return (
         <div>
-
+            <div className="grid md:grid-cols-2 gap-10">
+                {
+                    items.map(item => <Menu
+                        key={item._id}
+                        item={item}
+                    ></Menu>)
+                }
+            </div>
         </div>
     );
 };
